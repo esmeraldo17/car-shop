0a60ab307db3cac43adacf403fe21d0ce44c9bc0
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-05-14 19:26:03 +0100

    feat: interface e classe de motorcycle ciados

---
+++ src/Domains/Motorcycle.ts
@@ -0,0 +1,13 @@
+import IMotorcycle from '../Interfaces/IMotorcycle';
+import Vehicle from './Vehicle';
+
+export default class Motorcycle extends Vehicle {
+  private category: string;
+  private engineCapacity: number;
+
+  constructor(moto: IMotorcycle) {
+    super(moto);
+    this.category = moto.category;
+    this.engineCapacity = moto.engineCapacity;
+  }
+}
